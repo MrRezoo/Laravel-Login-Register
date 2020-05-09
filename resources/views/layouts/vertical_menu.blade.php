838df958eedd24cd9ec1a8f846021a71c9f16be0
Reza Mobaraki <rezam578@gmail.com>
2020-05-09 21:34:12 +0430

    first commit

---
+++ resources/views/layouts/vertical_menu.blade.php
@@ -0,0 +1,16 @@
+<div class="vertical-menu-main">
+    <nav id="main-nav">
+        <!-- Sample menu definition-->
+        <ul class="sm pixelstrap" id="main-menu">
+            <li>
+                <div class="text-right mobile-back">بازگشت<i class="fa fa-angle-right pl-2" aria-hidden="true"></i></div>
+            </li>
+            <li><a href="#"><i class="font-primary" data-feather="edit"></i>اطلاع رسانی</a>
+                <ul>
+                    <li><a href="{{route('notification.form.email')}}">ارسال ایمیل</a></li>
+                    <li><a href="{{route('notification.form.sms')}}">ارسال پیام کوتاه</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</div>
